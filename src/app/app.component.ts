1ae3eee3827a7480413f5e0c31088cf55041ae84
Joao Cortes <joaoscortes@gmail.com>
2021-06-04 19:24:13 +0100

    feat (product example)

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,6 @@
+import { HttpClient } from '@angular/common/http';
 import { Component } from '@angular/core';
+import { FormControl, FormGroup } from '@angular/forms';
 
 @Component({
   selector: 'app-root',
@@ -7,4 +9,34 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'StockClient';
+
+  powers = ["Power 1", "Power 2"]
+
+  productForm = new FormGroup({
+    iva: new FormControl(''),
+    pvp: new FormControl(''),
+    discount: new FormControl(''),
+  });
+
+  productValue = "";
+
+  constructor(
+    private http : HttpClient
+  ) {}
+
+  setProductValue(product: any) {
+    this.productValue = JSON.stringify(product);
+  }
+
+  createProduct() {
+    this.http.post("http://localhost:8080/stockmanagement/api/products", this.productForm.value).subscribe(
+      (res:any) => this.setProductValue(res)
+    )
+  }
+
+  getAllProducts() {
+    this.http.get("http://localhost:8080/stockmanagement/api/products").subscribe(
+      (res:any) => console.log(res)
+    )
+  }
 }
